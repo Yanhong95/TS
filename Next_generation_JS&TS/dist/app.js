55aca5c0dea53168a9da5ede7c271aa185d7cfb9
Yanhong95 <yanhongmain@gmail.com>
2020-10-28 22:02:51 -0400

    Next_generation_JS&TS

---
+++ Next_generation_JS&TS/dist/app.js
@@ -0,0 +1,28 @@
+"use strict";
+const add1 = (a, b = 1) => a + b;
+const printOutput = output => console.log(output);
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+printOutput(add1(5));
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+activeHobbies.push(...hobbies);
+const person = {
+    firstName: 'Max',
+    age: 30
+};
+const copiedPerson = Object.assign({}, person);
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
+//# sourceMappingURL=app.js.map
